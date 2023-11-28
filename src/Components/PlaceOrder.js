8c50d0cbc7d8b91c4452cc4e15ce38a062702b62
pravesh <pravesh.meena2892@gmail.com>
2023-11-28 10:06:32 +0530

    place order done

--- src/Components/PlaceOrder.js
+++ src/Components/PlaceOrder.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Flex,Text, Button} from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import placed from '../Assets/order-succesfull.gif'
 
 export default function PlaceOrder() {
+  const [userInfo, setUserInfo] = useState([]);
+  
+ 
+
+  useEffect(() => {
+    const user = localStorage.getItem("signup");
+    if (user) {
+      const parseData = JSON.parse(user);
+      setUserInfo(parseData);
+    }
+  }, []);
+
   return (
     <div>
         <Flex style={{marginTop:"10rem", flexDirection:"column", alignItems:"center"}}>
             <img src={placed} alt='orderplaced' style={{width:"20%"}}/>
-            <Text style={{color:"black", fontWeight:"600"}}>"Payment processed! Your order is now being prepared."</Text>
+            
+            <Text style={{color:"black", fontWeight:"600"}}>"Thanku ' {userInfo?.signup?.data?.name} ' Payment processed! Your order is now being prepared."</Text>
             <Link to='/'>
             <Button 
             style={{ width:"13rem", height:"2.5rem", fontWeight:"bold",fontSize:"18px",marginTop:"1.5rem",
